5f8ce67768cf94a6e88f9520a9af7d0e645a1397
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-29 13:32:57 +0530

    Added to PasswordThree: 1 numeric value

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -13,6 +13,7 @@
                 Console.WriteLine("4. Enter Mobile number along with country code");
                 Console.WriteLine("5. Minimum 8 character password");
                 Console.WriteLine("6. Password previous condition with atleast 1 upper case letter");
+                Console.WriteLine("7. Password previous conditions with atleast 1 numeric number");
                 Console.WriteLine("0. Exit");
                 Console.Write("Enter your choice : ");
                 int choice = Convert.ToInt32(Console.ReadLine());
@@ -42,6 +43,10 @@
                     case 6:
                         Validate validate6 = new Validate();
                         Console.WriteLine(validate6.Password2());
+                        break;
+                    case 7:
+                        Validate validate7 = new Validate();
+                        Console.WriteLine(validate7.Password3());
                         break;
                     case 0:
                         return;
