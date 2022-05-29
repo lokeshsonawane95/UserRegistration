6a4c019a8cd6283b698a1fdfc35f958eb54a7c92
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-29 13:18:12 +0530

    Added to ValidFirstName

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("\t\t\t\t\tWelcome to User Registration Problem");
+            while (true)
+            {
+                Console.WriteLine("\n1. Enter valid First Name");
+                
+                Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Validate validate1 = new Validate();
+                        Console.WriteLine(validate1.Name("First"));
+                        break;
+                    
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Please enter correct choice");
+                        break;
+                }
+            }
         }
     }
 }
